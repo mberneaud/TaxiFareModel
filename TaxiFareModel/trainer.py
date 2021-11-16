403f21e457069eaffc4a044ad01b8b9c15400c4f
Malte Berneaud <malte.berneaud@gmail.com>
2021-11-16 15:41:46 +0100

    remote API logging with MLFlow enabled

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -7,9 +7,16 @@ from sklearn.ensemble import RandomForestRegressor
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from TaxiFareModel.utils import compute_rmse
 from TaxiFareModel.data import get_data, clean_data
+from memoized_property import memoized_property
+import mlflow
+from mlflow.tracking import MlflowClient
 
 
 class Trainer():
+
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    EXPERIMENT_NAME = "[GER] [Berlin] [mberneaud] test_model v2"  # 🚨 replace with your country code, city, github_nickname and model name and version
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -19,7 +26,6 @@ class Trainer():
         self.fitted_pipe = None
         self.X = X
         self.y = y
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y)
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -43,12 +49,11 @@ class Trainer():
                     feat_eng_bloc), ('regressor', RandomForestRegressor())])
         return self.pipeline
 
-    def run(self, **fit_params):
+    def run(self, **kwargs):
         """set and train the pipeline"""
         self.fitted_pipe = self.set_pipeline()
-        self.fitted_pipe.fit(self.X_train, self.y_train, **fit_params)
+        self.fitted_pipe.fit(self.X, self.y, **kwargs)
         return self.fitted_pipe
-
 
 
     def evaluate(self, X_test, y_test):
@@ -56,6 +61,29 @@ class Trainer():
         y_pred = self.fitted_pipe.predict(X_test)
         self.model_score = compute_rmse(y_pred, y_test)
         return self.model_score
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.EXPERIMENT_NAME).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 
@@ -69,4 +97,6 @@ if __name__ == "__main__":
     trainer.set_pipeline()  # pipeline creation
     trainer.run()  # running, model stored in instance
     score = trainer.evaluate(X_test, y_test)# evaluate
-    print(f"the model score is:{score:.4f}")
+    trainer.mlflow_log_param("model", "random forest")
+    trainer.mlflow_log_metric("rmse", score)
+    print(f"the model rmse is:{score:.4f}")
